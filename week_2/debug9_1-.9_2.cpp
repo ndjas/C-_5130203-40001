8c039a3b0ab496c270dc11f5ca69fa22dc93002c
ndjas <kemajoufred@gmail.com>
2024-10-12 18:11:06 +0100

    add debug exercice

---
+++ week_2/debug9_1-.9_2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+#include <math.h>
+
+#define _USE_MATH_DEFINES
+
+using namespace std;
+
+int main() {
+
+        /*  9.1
+
+        int low, high, sum;
+		std::cout << "Enter low and high bound" << std::endl;
+		std::cin >> low >> high;
+		//сформироыать границы диапазона
+
+		//проверить корректность значений
+		if(low <= high)
+		{
+			// вычислить сумму
+			for(int i = low, sum = 0; i <= high; i++){
+				sum += i;
+				std::cout<<"sum = "<<sum<<std::endl;
+			}	
+			
+		}		
+		
+		// проверить в отладчике значение
+		
+
+*/
+
+/* 9.2
+
+              int x;
+		do
+		{
+			std::cout << "Enter digit >= 10 && even for exit " << std::endl;
+			std::cin >> x;
+		} 
+		while(x < 10);
+
+		std::cout<<"x = "<<x<<std::endl;
+
+*/
+  
+
+
+
+
+    return 0;
+}
